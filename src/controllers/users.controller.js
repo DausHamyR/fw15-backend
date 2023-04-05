5afeb253317d40cc3613d25912ece0eca4990be9
DausHamyR <daus.amat354@gmail.com>
2023-04-06 06:33:15 +0700

    menghubungkan database postgresql

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,38 +1,70 @@
-// const { request, response } = require("express")
+const userModel = require("../models/users.model")
 
-// const { response } = require("express")
-
-exports.getAllUsers = (request, response) => {
-    const data = [
-        {
-            name: "Bob",
-            phone: "085383262"
-        }
-    ]
-    return response.json({
-        success: true,
-        message: "List of all users",
-        results: data
+exports.getAllUsers = async(request, response) => {
+    const data = await userModel.findAll()
+    if(data){
+        return response.json({
+            success: true,
+            message: "List of all users",
+            results: data
+        })
+    }
+    return response.status(404).json({
+        success: false,
+        message: "Error: List all users not found",
     })
 }
 
-exports.createUser = (request, response) => {
-    return response.json({
-        success: true,
-        message: `Create user ${request.body.fullname} successfully`
+exports.getOneUser = async(request, response) => {
+    const data = await userModel.findOne(request.params.id)
+    if(data){
+        return response.json({
+            success: true,
+            message: "Detail user",
+            results: data
+        })
+    }
+    return response.status(404).json({
+        success: false,
+        message: "Error: user not found",
     })
 }
 
-exports.updateUser = (request, response) => {
-    return response.json({
-        success: true,
-        message: `Update user ${request.params.id} successfully`
+exports.createUser = async (request, response) => {
+    const data = await userModel.insert(request.body)
+    if(data){
+        return response.json({
+            success: true,
+            message: `Create user ${request.body.email} successfully`,
+            result: data
+        })
+    }
+    return response.status(404).json({
+        success: false,
+        message: `Errpr: user ${request.body.email} not found`,
     })
 }
 
-exports.deleteUser = (request, response) => {
+exports.updateUser = async (request, response) => {
+    const data = await userModel.update(request.params.id, request.body)
+    if(data){
+        return response.json({
+            success: true,
+            message: "Update user successfully",
+            results: data
+        })
+    }
+    return response.status(404).json({
+        success: false,
+        message: "Error user not found",
+    })
+}
+
+exports.deleteUser = async (request, response) => {
+    const data = await userModel.destroy(request.params.id)
     return response.json({
         success: true,
-        message: `Delete user ${request.params.id} successfully`
+        message: "Delete user successfully",
+        result: data
     })
 }
