8e0f628c7f51487023f6f9743092bf3bb9ed367a
DausHamyR <daus.amat354@gmail.com>
2023-05-14 14:55:05 +0700

    repair

--- src/controllers/MAIN BUSINESS FLOW/profile.controller.js
+++ src/controllers/MAIN BUSINESS FLOW/profile.controller.js
@@ -17,7 +17,8 @@ exports.updateProfile = async (request, response) => {
             if(user.picture) {
                 fileRemover({filename: user.picture})
             }
-            data.picture = request.file.filename
+            data.picture = request.file.path
+            // data.picture = request.file.filename
         }
         const profile = await profileModel.updateByUserId(id, data)
         if(!profile) {
